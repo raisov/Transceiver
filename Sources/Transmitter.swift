193c1eadc64e36a3b6b64918f5fa2ea281dc8a61
Vladimir Raisov <vladimir.raisov@proekspert.ee>
2025-01-20 23:10:12 +0200

    refactor (Transmitter): move the reply handling setting to the delegate setting

--- Sources/Transmitter.swift
+++ Sources/Transmitter.swift
@@ -9,33 +9,103 @@ import struct Foundation.Data
 import Sockets
 import Interfaces
 
+public enum TransmitterError: Error {
+	case notReady
+	case interfaceNotFound
+}
+
 public class Transmitter {
     private let socket: Socket
-    private var source: DispatchSourceRead
+    private var source: DispatchSourceRead?
 
-    public var delegate: DatagramHandler? {
-        willSet {
-            guard self.delegate != nil else {return}
-            self.source.suspend()
-        }
-        didSet {
-            guard let handler = delegate else {
-				source.cancel()
-				return}
-            source.setEventHandler {[handler, source] in
-                let handle = Int32(source.handle)
-                let length = Int(source.data)
-                do {
-                    handler.dataDidRead(try Datagram(handle,
-                                                     maxDataLength: length,
-                                                     maxAncillaryLength: 72))
-                } catch {
-                    handler.errorDidOccur(error)
-                }
-            }
-            source.resume()
-        }
-    }
+	public var delegate: DatagramHandler? {
+		willSet {
+			source?.cancel()
+			guard let newValue else { return }
+
+			do {
+				guard let localAddress = socket.localAddress else {
+					throw TransmitterError.notReady
+				}
+				guard let remoteAddress = socket.remoteAddress else {
+					throw TransmitterError.notReady
+				}
+
+				assert(remoteAddress.sin != nil && localAddress.sin != nil ||
+					   remoteAddress.sin6 != nil && localAddress.sin6 != nil)
+
+				var localSocket: Socket?
+
+				if let local_sin = localAddress.sin, let sin = remoteAddress.sin {
+					localSocket = try Socket(family: .inet, type: .datagram)
+					guard let interface = Self.findInterface(for: local_sin) else {
+						throw TransmitterError.interfaceNotFound
+					}
+					if sin.sin_addr == in_addr.broadcast || sin.sin_addr == interface.broadcast {
+						try socket.enable(option: SO_BROADCAST, level: SOL_SOCKET)
+						try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
+						try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
+						try localAddress.withSockaddrPointer(localSocket!.bind)
+					} else if sin.isMulticast {
+						try socket.enable(option: IP_MULTICAST_LOOP, level: IPPROTO_IP)
+						try socket.set(option: IP_MULTICAST_IFINDEX, level: IPPROTO_IP, value: interface.index)
+						try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
+						try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
+						try localAddress.withSockaddrPointer(localSocket!.bind)
+					}
+					try localSocket!.enable(option: IP_RECVIF, level: IPPROTO_IP) // 32
+					try localSocket!.enable(option: IP_RECVDSTADDR, level: IPPROTO_IP) // 16
+					try localSocket!.enable(option: IP_RECVPKTINFO, level: IPPROTO_IP) // 24
+				} else if let local_sin6 = localAddress.sin6, let sin6 = remoteAddress.sin6 {
+					localSocket = try Socket(family: .inet6, type: .datagram)
+					guard let interface = Self.findInterface(for: local_sin6) else {
+						throw TransmitterError.interfaceNotFound
+					}
+					if sin6.isMulticast {
+						try socket.set(option: IPV6_MULTICAST_IF, level: IPPROTO_IPV6, value: interface.index)
+						try socket.enable(option: IPV6_MULTICAST_LOOP, level: IPPROTO_IPV6)
+						try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
+						try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
+						try localAddress.withSockaddrPointer(localSocket!.bind)
+					}
+					try localSocket!.enable(option: IPV6_2292PKTINFO, level: IPPROTO_IPV6)
+				}
+
+				guard let localSocket else {
+					throw TransmitterError.notReady
+				}
+
+				let handle = try localSocket.duplicateDescriptor()
+				source = DispatchSource.makeReadSource(fileDescriptor: handle)
+				source!.setCancelHandler{
+					close(Int32(handle))
+				}
+			} catch {
+				delegate?.errorDidOccur(error)
+			}
+
+		}
+		didSet {
+			guard let source, let delegate else { return }
+
+			source.setEventHandler {
+				let handle = Int32(source.handle)
+				let length = Int(source.data)
+				do {
+					delegate.dataDidRead(
+						try Datagram(
+							handle,
+							maxDataLength: length,
+							maxAncillaryLength: 72
+						)
+					)
+				} catch {
+					delegate.errorDidOccur(error)
+				}
+			}
+			source.activate()
+		}
+	}
 
     /// Creates transmitter to send unicast, multicast and broadcast datagram
     /// - Parameters:
@@ -69,55 +139,12 @@ public class Transmitter {
         
         self.socket.nonBlockingOperations = true
         try address.withSockaddrPointer(self.socket.connectTo)
-    
-    
-        guard let localAddress = self.socket.localAddress else { return nil }
-        
-        assert(address.sin != nil && localAddress.sin != nil ||
-               address.sin6 != nil && localAddress.sin6 != nil)
-        
-        var localSocket: Socket?
-        
-        if let local_sin = localAddress.sin, let sin = address.sin {
-            localSocket = try Socket(family: .inet, type: .datagram)
-            guard let interface = Self.findInterface(for: local_sin) else { return nil }
-            if sin.sin_addr == in_addr.broadcast || sin.sin_addr == interface.broadcast {
-                try socket.enable(option: SO_BROADCAST, level: SOL_SOCKET)
-                try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
-                try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
-                try localAddress.withSockaddrPointer(localSocket!.bind)
-            } else if sin.isMulticast {
-                try socket.enable(option: IP_MULTICAST_LOOP, level: IPPROTO_IP)
-                try socket.set(option: IP_MULTICAST_IFINDEX, level: IPPROTO_IP, value: interface.index)
-                try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
-                try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
-                try localAddress.withSockaddrPointer(localSocket!.bind)
-            }
-            try localSocket!.enable(option: IP_RECVIF, level: IPPROTO_IP) // 32
-            try localSocket!.enable(option: IP_RECVDSTADDR, level: IPPROTO_IP) // 16
-            try localSocket!.enable(option: IP_RECVPKTINFO, level: IPPROTO_IP) // 24
-        } else if let local_sin6 = localAddress.sin6, let sin6 = address.sin6 {
-            localSocket = try Socket(family: .inet6, type: .datagram)
-            guard let interface = Self.findInterface(for: local_sin6) else { return nil }
-            if sin6.isMulticast {
-                try socket.set(option: IPV6_MULTICAST_IF, level: IPPROTO_IPV6, value: interface.index)
-                try socket.enable(option: IPV6_MULTICAST_LOOP, level: IPPROTO_IPV6)
-                try localSocket!.enable(option: SO_REUSEADDR, level: SOL_SOCKET)
-                try localSocket!.enable(option: SO_REUSEPORT, level: SOL_SOCKET)
-                try localAddress.withSockaddrPointer(localSocket!.bind)
-            }
-            try localSocket!.enable(option: IPV6_2292PKTINFO, level: IPPROTO_IPV6)
-        }
-        
-        guard let localSocket else { return nil }
-        
-        let handle = try localSocket.duplicateDescriptor()
-        source = DispatchSource.makeReadSource(fileDescriptor: handle)
-        source.setCancelHandler{[handle = source.handle] in
-            Darwin.close(Int32(handle))
-        }
-    }
-    
+	}
+
+	deinit {
+		source?.cancel()
+	}
+
     // MARK: - Public methods
 
     /// Sends datagram to destination specified when transmitter created.
